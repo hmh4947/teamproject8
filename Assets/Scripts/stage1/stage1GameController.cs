80c8102e123c55805a785750927995630c856989
wonyongLee123 <night98200@gmail.com>
2023-06-04 15:02:44 +0900

    230604 Adding Stage 1
    Stage 1 added but not complete

--- Assets/Scripts/stage1/stage1GameController.cs
+++ Assets/Scripts/stage1/stage1GameController.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gameMain : MonoBehaviour
+public class stage1GameController : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     void Start()
     {
