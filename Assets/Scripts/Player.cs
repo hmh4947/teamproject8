71887be4403944ac395b775379aacd6a15a74c74
hmh4947 <hmh4947@naver.com>
2023-06-22 00:32:20 +0900

    jump ani

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,7 @@ public class Player : MonoBehaviour
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
+    bool isJumping = false;
     // Start is called before the first frame update
     void Awake()
     {
@@ -19,19 +20,20 @@ public class Player : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
 
-       // anim.SetBool("isJumping", false);
+       
     }
 
     void Update()
     {
+      
         //Jump
         if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             anim.SetBool("isJumping", true);
         }
-            
-            //Stop Speed
+        
+        //Stop Speed
         if (Input.GetButtonUp("Horizontal"))
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
             
@@ -49,6 +51,7 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
+        
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
@@ -57,25 +60,25 @@ public class Player : MonoBehaviour
         else if (rigid.velocity.x < maxSpeed * (-1))//Left Max Speed
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
 
-        if (rigid.velocity.y < 0)
+        if (rigid.velocity.y < 0 && anim.GetBool("isJumping"))
         {
             Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
             RaycastHit2D rayhit = Physics2D.Raycast(rigid.position, Vector3.down, 0.5f, LayerMask.GetMask("Platform"));
+ 
+         if (rayhit.collider != null)
+            {
+          //if (rayhit.distance < 0.5f)
+             anim.SetBool("isJumping", false);
+                
+            }
 
-
-            
-                if (rayhit.collider != null)
-                {
-                    if (rayhit.distance < 0.5f)
-                        anim.SetBool("isJumping", false);
-                }
-
-            
+             anim.SetBool("isJumping", false);
         }
 
-        anim.SetBool("isJumping", false);
+       
 
     }
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
